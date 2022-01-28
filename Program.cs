afd7fb7ee0f4da616c1c2483a3219cdcdd10b244
hnguy <hnguy@DESKTOP-3290PJ2>
2022-01-27 22:49:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my favorite quote:");
+            Console.WriteLine("Taking on a challenge is a lot like riding a horse, isn't it? If you're comfortable while you're doing it, you're probably doing it wrong");
+            Console.WriteLine("Submitted by Hieu Nguyen");
+        }
+    }
+}
